9ca5acdc00451a3fdc772fbe1889bf3643b97d8c
Yves NDJ <yndjamba@live.fr>
2020-09-20 21:43:23 +0200

    clients add and delete

--- src/clients/App.js
+++ src/clients/App.js
@@ -5,24 +5,43 @@ import './App.css';
 
 class Appi extends React.Component {
     
+    
     state = {
         clients: [
         {id: 1, nom: "Monsieur 1"},
         {id: 2, nom: "Monsieur 2"},
         {id: 3, nom: "Monsieur 3"}
         ],
-
+        nouveauClient: "",
     };
     
     handleDelete = (id) => {
         const clients = this.state.clients.slice(); // Faire une copie du tableau
-        const index = clients.findIndex(function(client){
-            return client.id === id
-        })
+        const index = clients.findIndex((client) => client.id === id)
         clients.splice(index,1);
         
-        this.setState({clients: clients})
+        this.setState({clients: clients});
     }
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const id = new Date().getTime();
+        const nom =  this.state.nouveauClient;
+        
+        const client = {id: id, nom: nom}
+        
+        const clients = this.state.clients.slice();
+        clients.push(client);
+        
+        this.setState({clients: clients, nouveauClient:""});
+    }
+    
+    handleChange = (e) => {
+        this.setState({nouveauClient: e.currentTarget.value})
+    }
+    
+    
 
     render(){
     return (
@@ -35,8 +54,8 @@ class Appi extends React.Component {
             </li> 
         ))}
         </ul>
-        <form>
-        <input type="text"/><button>Valider</button>
+        <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChange} value={this.state.nouveauClient} placeholder="Ajouter client" type="text"/><button>Valider</button>
         </form>
     </div>
         );
