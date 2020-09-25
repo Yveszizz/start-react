014f54fa996f47c22463f76913092bfac9ca74c0
Yves NDJ <yndjamba@live.fr>
2020-09-25 11:39:06 +0200

    change persons names

--- src/App.js
+++ src/App.js
@@ -19,24 +19,42 @@ const App = props => {
     
     console.log(personsState, otherState);
     
-const switchNameHandler = () => {
-    setPersonsState({
-        persons: [
-            {name:'Max', age:'28'},
-            {name:'Manu', age:'29'},
-            {name:'Stephanie', age:'29'}
-        ]})
-}
+    const switchNameHandler = (newName) => {
+        setPersonsState({
+            persons: [
+                {name: newName, age:'28'},
+                {name:'Manu', age:'29'},
+                {name:'Stephanie', age:'29'}
+            ]})
+    }
 
+    const nameChangedHandler = (e) => {
+        setPersonsState({
+            persons: [
+                {name: 'Max', age:'28'},
+                {name: e.target.value, age:'29'},
+                {name:'Stephanie', age:'26'}
+                ]
+        })
+    }
+    
     return (
       <div className="App">
         <Appo />
         <Appi />
-        <button onClick={switchNameHandler}>Switch</button>
+        <button onClick={switchNameHandler.bind(this,'Yves')}>Switch</button>
         <div className="div-persons">
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>I love soccer</Person>
+            <Person 
+            name={personsState.persons[0].name} 
+            age={personsState.persons[0].age} />
+            <Person 
+            changed={nameChangedHandler}
+            name={personsState.persons[1].name} 
+            age={personsState.persons[1].age} />
+            <Person
+            click={switchNameHandler.bind(this,'Yvesziz')}
+            name={personsState.persons[2].name} 
+            age={personsState.persons[2].age}>I love soccer</Person>
         </div>
       </div>
     );
--- src/personnes/App.js
+++ src/personnes/App.js
@@ -4,9 +4,10 @@ import './App.css';
  const person = (props) => {
     return (
     <div className="person">
-        <h1>{props.name}</h1>
+        <h1 onClick={props.click} >{props.name}</h1>
         <p>Your age : {props.age} </p>
         <p>{props.children}</p>
+        <input type='text' onChange={props.changed} value={props.name}/>
     </div>
     );
 }
